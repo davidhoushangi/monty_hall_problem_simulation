8b998f30596b25332230a9d302cfd5274bb91e18
DAVID HOUSHANGI <davidhoushangi@berkeley.edu>
2026-09-19 23:56:59 -0500

    Create dashboard.py

---
+++ dashboard.py
@@ -0,0 +1,38 @@
+import streamlit as st
+from src.monty_hall_problem import monty_hall_game
+import time
+
+st.title(":zap: Monty Hall Simulation")
+
+num_games = st.number_input(
+    "Enter number of games to simulate",
+    min_value=1, max_value=10000, 
+    value=100
+)
+
+col1, col2 = st.columns(2)
+col1.subheader('Win Percentage without Switching')
+col2.subheader('Win Percentage with Switching')
+
+chart1_placeholder = col1.empty()
+chart2_placeholder = col2.empty()
+
+if st.button("Run Simulation"):
+    wins_no_switch = 0
+    wins_switch = 0
+    data_no_switch = []
+    data_switch = []
+
+    for i in range(num_games):
+        if monty_hall_game(False):
+            wins_no_switch += 1
+        if monty_hall_game(True):
+            wins_switch += 1
+
+        data_no_switch.append(wins_no_switch / (i + 1))
+        data_switch.append(wins_switch / (i + 1))
+
+        chart1_placeholder.line_chart(data_no_switch)
+        chart2_placeholder.line_chart(data_switch)
+
+        time.sleep(0.01)
